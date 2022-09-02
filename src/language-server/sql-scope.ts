1e08bfe265321ad54370c8e01187d43972a1fce1
Miro Spönemann <miro.spoenemann@typefox.io>
2022-09-02 15:20:55 +0200

    Initial commit

---
+++ src/language-server/sql-scope.ts
@@ -0,0 +1,45 @@
+import {
+    AstNode, AstNodeDescriptionProvider, DefaultScopeProvider, EMPTY_SCOPE, LangiumServices, ReferenceInfo,
+    Scope, stream, StreamScope
+} from 'langium';
+import { isColumnReference, isSelectQuery, TableDefinition } from './generated/ast';
+
+export class SqlScopeProvider extends DefaultScopeProvider {
+
+    private readonly astNodeDescriptionProvider: AstNodeDescriptionProvider;
+
+    constructor(services: LangiumServices) {
+        super(services);
+        this.astNodeDescriptionProvider = services.workspace.AstNodeDescriptionProvider;
+    }
+
+    getScope(context: ReferenceInfo): Scope {
+        if (isColumnReference(context.container) && context.property === 'column') {
+            return this.getColumnReferenceScope(context);
+        }
+        return super.getScope(context);
+    }
+
+    private getColumnReferenceScope(context: ReferenceInfo): Scope {
+        let container: AstNode | undefined = context.container;
+        let contProp: string | undefined = undefined;
+        while (container) {
+            contProp = container.$containerProperty;
+            container = container.$container;
+            if (isSelectQuery(container) && contProp === 'columns') {
+                const table = container.table.ref;
+                if (table) {
+                    return this.getTableColumnsScope(table);
+                }
+            }
+        }
+        return EMPTY_SCOPE;
+    }
+
+    private getTableColumnsScope(table: TableDefinition): Scope {
+        return new StreamScope(
+            stream(table.columns).map(c => this.astNodeDescriptionProvider.createDescription(c, c.name))
+        );
+    }
+
+}
---
+++ src/language-server/sql-validator.ts
@@ -0,0 +1,30 @@
+import { ValidationAcceptor, ValidationChecks, ValidationRegistry } from 'langium';
+import { SelectQuery, SqlAstType } from './generated/ast';
+import type { SqlServices } from './sql-module';
+
+/**
+ * Registry for validation checks.
+ */
+export class SqlValidationRegistry extends ValidationRegistry {
+    constructor(services: SqlServices) {
+        super(services);
+        const validator = services.validation.SqlValidator;
+        const checks: ValidationChecks<SqlAstType> = {
+            SelectQuery: validator.checkSelectQuery
+        };
+        this.register(checks, validator);
+    }
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class SqlValidator {
+
+    checkSelectQuery(query: SelectQuery, accept: ValidationAcceptor): void {
+        if (query.table?.ref && query.columns.length === 0 && !query.wildcard) {
+            accept('error', 'Query must specify at least one output column.', { node: query })
+        }
+    }
+
+}
