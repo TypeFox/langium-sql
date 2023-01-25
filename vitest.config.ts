030f465322e643e1d02a2fdd0e16b4a9ededabdf
Markus Rudolph <markus.rudolph@typefox.io>
2023-01-25 16:33:10 +0100

    Avada Kedavra, bug!

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
+        include: ['src/tests/**/*.test.ts'],
         globals: true,
         deps: {
             interopDefault: true,
