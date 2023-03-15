a6b5397a25d0e4e8dbd43afa3dc818d10ba41031
Mark Sujew <mark.sujew@typefox.io>
2023-03-15 16:48:33 +0000

    Fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,20 @@
  * This program and the accompanying materials are made available under the
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
-const path = require("path");
-const headerDef = path.resolve(__dirname, "header.js");
 
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "header"],
-  rules: {
-    "header/header": [2, headerDef],
-  },
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint", "header"],
+    rules: {
+        "header/header": [
+            2,
+            "block",
+            { "pattern": "MIT License|DO NOT EDIT MANUALLY!" }
+        ]
+    }
 };
