af2771afab1ae1b33ebcb12c3dddd7b51e269fa6
Markus Rudolph <markus.rudolph@typefox.io>
2024-03-11 15:56:32 +0100

    Fix esbuild

--- packages/langium-sql-vscode/esbuild.mjs
+++ packages/langium-sql-vscode/esbuild.mjs
@@ -23,43 +23,47 @@ function padZeroes(i) {
     return i.toString().padStart(2, '0');
 }
 
-esbuild.build({
+const ctxNode = await esbuild.context({
     // Two entry points, one for the extension, one for the language server
     entryPoints: ['src/node/extension.ts', 'src/node/language-server.ts'],
     outdir: 'dist/node',
+    outExtension: {
+        '.js': '.cjs'
+    },
     bundle: true,
+    format: 'cjs',
     loader: { '.ts': 'ts' },
     external: ['vscode'], // the vscode-module is created on-the-fly and must be excluded.
     platform: 'node', // VSCode extensions run in a node process
     sourcemap: !minify,
-    watch: watch ? {
-        onRebuild(error) {
-            if (error) console.error(`${getTime()}Node Watch build failed`)
-            else console.log(`${getTime()}Node ${success}`)
-        }
-    } : false,
     minify
-})
-    .then(() => console.log(`${getTime()}Node ${success}`))
-    .catch(() => process.exit(1));
-
-esbuild.build({
+});
+const ctxBrowser = await esbuild.context({
     // Two entry points, one for the extension, one for the language server
     entryPoints: ['src/browser/extension.ts', 'src/browser/language-server.ts'],
     outdir: 'dist/browser',
+    outExtension: {
+        '.js': '.cjs'
+    },
     bundle: true,
     loader: { '.ts': 'ts' },
     external: ['vscode'], // the vscode-module is created on-the-fly and must be excluded.
     platform: 'browser', // VSCode extensions run in a node process,
     format: 'iife',
     sourcemap: !minify,
-    watch: watch ? {
-        onRebuild(error) {
-            if (error) console.error(`${getTime()}Browser Watch build failed`)
-            else console.log(`${getTime()}Browser ${success}`)
-        }
-    } : false,
     minify
-})
-    .then(() => console.log(`${getTime()}Browser ${success}`))
-    .catch(() => process.exit(1));
+});
+
+if (watch) {
+    await Promise.all([
+        ctxNode.watch(),
+        ctxBrowser.watch()
+    ]);
+} else {
+    await Promise.all([
+        ctxNode.rebuild(),
+        ctxBrowser.rebuild()
+    ]);
+    ctxNode.dispose();
+    ctxBrowser.dispose();
+}
